fb8c1e2d662a8a95362dfb5bfc6d0ff52b6e16f7
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-03-27 19:42:41 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
-from Questgen import main
+import main
 
 payload = {
     "input_text": """Computer vision is a field of artificial intelligence (AI) that enables computers and systems to derive meaningful information from digital images, videos and other visual inputs — and take actions or make recommendations based on that information. If AI enables computers to think, computer vision enables them to see, observe and understand.
