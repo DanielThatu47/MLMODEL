d602b7565f4c4ed0a6bdc70add363d292eb77ed5
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-03-27 19:42:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+# backend.py
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from Questgen.main import QGen
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all domains
+
+# Initialize the QGen object
+qgen = QGen()
+
+@app.route('/generate_questions', methods=['POST'])
+def generate_questions():
+    # Get the input text from the request
+    input_text = request.json.get('input_text')
+
+    # Generate MCQs
+    payload = {"input_text": input_text}
+    output = qgen.predict_mcq(payload)
+
+    # Prepare the response
+    response_data = {
+        "questions": [
+            {
+                "question_statement": question.get("question_statement"),
+                "options": question.get("options", []),
+                "answer": question.get("answer", "Correct answer not available")
+            }
+            for question in output["questions"]
+        ]
+    }
+
+    # Return the generated MCQs as JSON
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
